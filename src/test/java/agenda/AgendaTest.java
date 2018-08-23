e4b8a99ec7a1960380b047924b87bfef6d11a7e9
Sara <Sara@LAPTOP-R3UU04CI>
2018-08-23 17:36:44 +0200

    new commit

---
+++ src/test/java/agenda/AgendaTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sara
+ */
+public class AgendaTest {
+    
+    public AgendaTest() {
+    }
+    
+}
